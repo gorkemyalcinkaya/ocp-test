a176636af103177989519ec91822ad137ccd8c09
gorkemyalcinkaya <gorkemyalcinkaya7@gmail.com>
2023-01-02 09:24:13 +0300

    initial commit

---
+++ src/main/java/com/example/ocpdepltest/OcpDeplTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.ocpdepltest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OcpDeplTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OcpDeplTestApplication.class, args);
+	}
+
+}
